c9553528df0046caa094322aa3257e9e3617e0bf
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-28 13:11:13 -0300

    Endpoint with dollar prices using map and filter.

--- api/index.js
+++ api/index.js
@@ -1,19 +1,28 @@
 import fetch from "isomorphic-unfetch";
+import { DateTime } from "luxon";
 
 const URL_DOLLAR_PRICE = "https://challenge-api.aerolab.co/dollar";
 const URL_PRODUCTS = "https://challenge-api.aerolab.co/products";
 
+/**
+ * API endpoint for extend Aerolab API with dollar prices
+ */
 export default async (req, res) => {
   const dolarData = await fetch(URL_DOLLAR_PRICE);
   const data = await dolarData.json();
   const dollar = data.rate;
   const page = req?.query?.page ?? "1";
   const products = await getProducts(page);
-  extendsModel(dollar, products);
+  const filterProducts = getUpdatedProducts(products);
+  const extendedProducts = extendsModel(dollar, filterProducts);
   res.statusCode = 200;
-  res.json({ products });
+  res.json({ extendedProducts });
 };
 
+/**
+ * Return products list from Aerolab api.
+ * @param {*} page
+ */
 async function getProducts(page) {
   let products;
   const res = await fetch(`${URL_PRODUCTS}?page=${page}`);
@@ -21,11 +30,30 @@ async function getProducts(page) {
   return products.products;
 }
 
+/**
+ * Adds dollar prices to products.
+ * @param {*} dollar
+ * @param {*} products
+ */
 function extendsModel(dollar, products) {
-  products.map((prod) => {
-    if (true) {
-      prod.dollarPrice = prod.price / dollar;
-      prod.dollarOriginalPrice = prod.originalPrice / dollar;
-    }
+  let ExtendedProducts = products.map((prod) => {
+    prod.dollarPrice = prod.price / dollar;
+    prod.dollarOriginalPrice = prod.originalPrice / dollar;
+    return prod;
   });
+  return ExtendedProducts;
+}
+
+/**
+ * Returns products list with products that were updated less than a month ago
+ * @param {} products
+ */
+function getUpdatedProducts(products) {
+  let filteredProducts = products.filter((prod) => {
+    let lastUpdate = DateTime.fromISO(prod.updatedAt);
+    let current = DateTime.local();
+    let diffInMonths = current.diff(lastUpdate, "months");
+    return diffInMonths.values.months < 1;
+  });
+  return filteredProducts;
 }
